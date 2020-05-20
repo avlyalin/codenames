e1cad0227f621e237a69f07f007aaef7723666f1
avlyalin <lyalin.alexey@gmail.com>
2020-05-20 23:24:38 +0300

    score bar visual tests failure threshold increased

--- src/components/scores-bar/scores-bar.ispec.js
+++ src/components/scores-bar/scores-bar.ispec.js
@@ -1,46 +1,51 @@
 /* eslint no-undef: 0 */
 
+const customConfig = {
+  failureThreshold: 0.01,
+  failureThresholdType: 'percent',
+};
+
 describe('<ScoresBar /> visually looks correct', () => {
   test('"vs" icon is red for user from red team', async () => {
     await page.goto(
       'http://localhost:9009/iframe.html?path=/story/scores-bar--with-opened-cards&knob-User team=red',
     );
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    expect(image).toMatchImageSnapshot(customConfig);
   });
   test('"vs" icon is blue for user from blue team', async () => {
     await page.goto(
       'http://localhost:9009/iframe.html?path=/story/scores-bar--with-opened-cards&knob-User team=blue',
     );
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    expect(image).toMatchImageSnapshot(customConfig);
   });
   test('with 0 opened cards', async () => {
     await page.goto(
       'http://localhost:9009/iframe.html?path=/story/scores-bar--no-opened-cards&knob-User team=blue',
     );
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    expect(image).toMatchImageSnapshot(customConfig);
   });
   test('with opened cards', async () => {
     await page.goto(
       'http://localhost:9009/iframe.html?path=/story/scores-bar--with-opened-cards&knob-User team=blue',
     );
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    expect(image).toMatchImageSnapshot(customConfig);
   });
   test('with all opened team cards', async () => {
     await page.goto(
       'http://localhost:9009/iframe.html?path=/story/scores-bar--with-all-opened-team-cards&knob-User team=blue',
     );
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    expect(image).toMatchImageSnapshot(customConfig);
   });
   test('with all opened cards', async () => {
     await page.goto(
       'http://localhost:9009/iframe.html?path=/story/scores-bar--with-all-opened-cards&knob-User team=blue',
     );
     const image = await page.screenshot();
-    expect(image).toMatchImageSnapshot();
+    expect(image).toMatchImageSnapshot(customConfig);
   });
 });
