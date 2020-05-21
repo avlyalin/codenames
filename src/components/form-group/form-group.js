c3f8fd5fc90346dbba3bff5f16f11f044e61fddb
avlyalin <lyalin.alexey@gmail.com>
2020-05-21 21:35:22 +0300

    refactor: form group styles

--- src/components/form-group/form-group.js
+++ src/components/form-group/form-group.js
@@ -1,48 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import styles from './form-group.module.css';
 
 const FormGroup = React.forwardRef(function FormGroup(props, ref) {
   const { label, labelFor, children, childrenContainerClass, ...other } = props;
   const childrenCount = React.Children.count(children);
+  const labelClasses = 'text-sm text-gray-400 mb-2';
   let content;
   if (!labelFor || childrenCount > 1) {
     content = (
       <fieldset>
-        <legend
-          className={classnames({
-            [styles.formGroupLabel]: true,
-          })}
-        >
-          {label}
-        </legend>
+        <legend className={labelClasses}>{label}</legend>
         <div className={childrenContainerClass}>{children}</div>
       </fieldset>
     );
   } else {
     content = (
       <>
-        <label
-          className={classnames({
-            [styles.formGroupLabel]: true,
-          })}
-          htmlFor={labelFor}
-        >
+        <label className={labelClasses} htmlFor={labelFor}>
           {label}
         </label>
         {children}
       </>
     );
   }
+
   return (
-    <div
-      className={classnames({
-        [styles.formGroup]: true,
-      })}
-      ref={ref}
-      {...other}
-    >
+    <div className="flex flex-col" ref={ref} {...other}>
       {content}
     </div>
   );
