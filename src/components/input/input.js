b274a67bc0977e7a6260fd424a503cbadcbbf9f7
avlyalin <lyalin.alexey@gmail.com>
2020-05-21 23:47:19 +0300

    refactor: input

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import styles from './input.module.css';
 
 const Input = React.forwardRef(function Input(props, ref) {
   const { classes = '', ...other } = props;
   return (
     <input
       ref={ref}
-      className={classnames({ [styles.input]: true, [classes]: classes })}
+      className={classnames(
+        'w-full h-10',
+        'p-3 py-1',
+        'text-base text-black',
+        'border border-solid border-gray-200 rounded-lg',
+        'focus:outline-none focus:shadow-outline-sm',
+        'disabled:bg-gray-100',
+        classes,
+      )}
       type="text"
       {...other}
     />
