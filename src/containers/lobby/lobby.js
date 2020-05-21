4bb592734a1d3fe44985b6ba8079e605d4059f0f
avlyalin <lyalin.alexey@gmail.com>
2020-05-22 01:37:25 +0300

    WIP: lobby page

--- src/containers/lobby/lobby.js
+++ src/containers/lobby/lobby.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { CARDS_DICTIONARIES, FIELD_SIZES, TEAMS } from 'src/data/constants';
 import { TeamSelect } from 'src/components/team-select';
 import { WithRadioGroup as Radio } from 'src/components/radio';
 import { RadioGroup } from 'src/components/radio-group';
-import styles from './lobby.module.css';
+import { InputGroup } from 'src/components/input-group';
+import { Input } from 'src/components/input';
+import { Button } from 'src/components/button';
+import { FormGroup } from 'src/components/form-group';
+import { Select } from 'src/components/select';
 
 function Lobby({
   sessionId,
@@ -24,94 +30,93 @@ function Lobby({
   const onChangeDictionary = (e) => {
     onChangeSettings({ ...settings, dictionary: e.target.value });
   };
-  const fieldSizes = Object.entries(FIELD_SIZES).map(([label, value]) => {
-    return (
-      <React.Fragment key={value}>
-        <label>
-          {label}
-          <input
-            type="radio"
-            name="field-size"
-            value={value}
-            checked={settings.fieldSize === value}
-            onChange={onChangeFieldSize}
-          />
-        </label>
-        <br />
-      </React.Fragment>
-    );
-  });
+  const fieldSizes = Object.entries(FIELD_SIZES).map(([label, value]) => (
+    <React.Fragment key={value}>
+      <Radio label={label} value={value} />
+    </React.Fragment>
+  ));
   const dictionaries = Object.entries(CARDS_DICTIONARIES).map(
-    ([label, value]) => {
-      return (
-        <React.Fragment key={value}>
-          <label>
-            {label}
-            <input
-              type="radio"
-              name="dictionary"
-              value={value}
-              checked={settings.dictionary === value}
-              onChange={onChangeDictionary}
-            />
-          </label>
-          <br />
-        </React.Fragment>
-      );
-    },
+    ([label, value]) => (
+      <option key={value} value={value}>
+        {label}
+      </option>
+    ),
   );
-  const usersList = users.map(({ id, name }) => {
-    if (currentUser.id === id) {
-      name += ' (вы)';
-    }
-    return <li key={id}>{name}</li>;
-  });
+
+  let color = 'default';
+  let bgColor = '';
+  if (currentUser.team === TEAMS['blue']) {
+    color = TEAMS['blue'];
+    bgColor = 'background-blue';
+  } else if (currentUser.team === TEAMS['red']) {
+    color = TEAMS['red'];
+    bgColor = 'background-red';
+  }
+
   return (
-    <div>
-      <section>
-        <h3>ID сессии:</h3>
-        {sessionId}
-      </section>
-      <section>
-        <h3>Размер поля:</h3>
-        {fieldSizes}
-      </section>
-      <section>
-        <h3>Словарь:</h3>
-        {dictionaries}
-      </section>
-      <section>
-        <h3>Игрок</h3>
-        <label>
-          Ваш ник:
-          <br />
-          <input
-            type="text"
+    <div className={classnames('p-5 pt-4 min-h-screen', bgColor)}>
+      <h1 className={'text-center text-xl uppercase tracking-wider'}>
+        Codenames
+      </h1>
+      <div className={'mt-4'}>
+        <FormGroup label={'ID сессии'}>
+          <InputGroup
+            append={
+              <Button classes="w-10" color={color} fullWidth={false}>
+                <FontAwesomeIcon icon="share-alt" size="lg" />
+              </Button>
+            }
+          >
+            <Input defaultValue={sessionId} disabled />
+          </InputGroup>
+        </FormGroup>
+      </div>
+
+      <div className={'mt-8'}>
+        <RadioGroup
+          color={color}
+          label="Размер поля"
+          value={settings.fieldSize}
+          name="field-size"
+          onChange={onChangeFieldSize}
+        >
+          {fieldSizes}
+        </RadioGroup>
+      </div>
+
+      <div className={'mt-8'}>
+        <FormGroup label={'Словарь'}>
+          <Select onChange={onChangeDictionary}>{dictionaries}</Select>
+        </FormGroup>
+      </div>
+
+      <div className={'mt-8'}>
+        <FormGroup label={'Ваш ник'} labelFor={'username'}>
+          <Input
+            id={'username'}
             value={currentUser.name}
             onChange={(e) => onChangeUsername(e.target.value)}
           />
-        </label>
-      </section>
-      <section>
-        <h3>Список игроков:</h3>
-        <ul>{usersList}</ul>
-      </section>
-      <section>
-        <h3>Команды</h3>
-        <div className={styles.teams}>
-          <TeamSelect
-            captains={captains}
-            currentUser={currentUser}
-            users={users}
-            captainId={captains[TEAMS['red']]}
-            onJoin={onJoinTeam}
-            onJoinAsCaptain={onJoinTeamAsCaptain}
-          />
-        </div>
-      </section>
-      <Link to="/game">
-        <button type="button">Играть!</button>
-      </Link>
+        </FormGroup>
+      </div>
+
+      <div className={'mt-8'}>
+        <TeamSelect
+          captains={captains}
+          currentUser={currentUser}
+          users={users}
+          onJoin={onJoinTeam}
+          onJoinAsCaptain={onJoinTeamAsCaptain}
+        />
+      </div>
+
+      <div className={'mt-8'}>
+        <Link to="/game">
+          <Button color={color}>
+            <FontAwesomeIcon icon="running" size="lg" /> Погнали
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
@@ -128,12 +133,12 @@ Lobby.propTypes = {
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
       team: PropTypes.string.isRequired,
-      role: PropTypes.string.isRequired,
-    }),
+    }).isRequired,
   ),
   currentUser: PropTypes.shape({
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    team: PropTypes.string.isRequired,
   }).isRequired,
   captains: PropTypes.shape({
     blue: PropTypes.string.isRequired,
---
+++ src/containers/lobby/lobby.stories.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { withKnobs, text, radios } from '@storybook/addon-knobs';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { containerDecorator } from '_storybook/container';
+import {
+  CARDS_DICTIONARIES,
+  FIELD_SIZES,
+  LANGUAGES,
+  TEAMS,
+} from 'src/data/constants';
+import { Lobby } from './lobby';
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  title: 'Containers/Lobby',
+  decorators: [
+    containerDecorator({
+      styles: { padding: 0 },
+    }),
+    withKnobs,
+  ],
+};
+
+export const Common = () => {
+  const sessionId = text('Session id', '_35gVe01Dsc');
+  const userTeam = radios(
+    'User team',
+    { default: '', blue: 'blue', red: 'red' },
+    '',
+  );
+  const settings = {
+    language: LANGUAGES['ru'],
+    fieldSize: FIELD_SIZES['5x5'],
+    dictionary: CARDS_DICTIONARIES['GAGA'],
+  };
+  const captains = {
+    [TEAMS['blue']]: '2',
+    [TEAMS['red']]: '6',
+  };
+  const user = {
+    id: '1',
+    name: 'Killer 3000',
+    team: userTeam,
+  };
+  const users = [
+    user,
+    { name: 'player_2', team: TEAMS['blue'], id: '2' },
+    { name: 'player_3', team: TEAMS['blue'], id: '3' },
+    { name: 'player_4', team: TEAMS['blue'], id: '4' },
+    { name: 'player_5', team: TEAMS['blue'], id: '5' },
+    { name: 'player_6', team: TEAMS['red'], id: '6' },
+  ];
+
+  return (
+    <BrowserRouter>
+      <Route>
+        <Lobby
+          captains={captains}
+          currentUser={user}
+          sessionId={sessionId}
+          settings={settings}
+          users={users}
+          onJoinTeam={() => {}}
+          onChangeSettings={() => {}}
+          onChangeUsername={() => {}}
+          onJoinTeamAsCaptain={() => {}}
+        />
+      </Route>
+    </BrowserRouter>
+  );
+};
